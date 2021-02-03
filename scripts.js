293eb88cc0abc88487294e94663074d4353eadfd
EmirlanDogdurbaev <jigit38@gmail.com>
2021-02-03 13:58:36 +0300

    done table

---
+++ scripts.js
@@ -0,0 +1,7 @@
+for (let i1 = 2; i1 <= 10; i1++) {
+    for (let i2 = 2; i2 <= 10; i2++) {
+ 
+     document.write( "<table><td>" + i1 + " * " + i2 + " = " + i1 * i2 + "</td></table>");
+ }
+ document.write("<br>")
+ }
